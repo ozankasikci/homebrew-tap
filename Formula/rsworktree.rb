e2b1b0551fe792d8e03d22c19b42ba5cd4c66753
Ozan Kaşıkçı <ozan@kasikci.io>
2025-09-26 17:52:55 +0300

    Use prebuilt arm64 binary

--- Formula/rsworktree.rb
+++ Formula/rsworktree.rb
@@ -1,17 +1,28 @@
 class Rsworktree < Formula
   desc "CLI helper for managing Git worktrees under a dedicated .rsworktree directory"
   homepage "https://github.com/ozankasikci/rust-git-worktree"
-  url "https://github.com/ozankasikci/rust-git-worktree/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "b85e1d87de6caf92d0b4a5965f60e55ff28da5b42cf8288852ff2aece8209756"
+  version "0.2.0"
   license "MIT"
   head "https://github.com/ozankasikci/rust-git-worktree.git", branch: "master"
 
-  depends_on "cmake" => :build
-  depends_on "pkg-config" => :build
-  depends_on "rust" => :build
+  if Hardware::CPU.arm?
+    url "https://github.com/ozankasikci/rust-git-worktree/releases/download/v0.2.0/rsworktree-v0.2.0-macos-arm64.tar.gz"
+    sha256 "a98c9f9b3bd9f6a77dd81b5b5f5674cdef04f331a3f21c66e5be8e9c7fce1e12"
+  else
+    url "https://github.com/ozankasikci/rust-git-worktree/archive/refs/tags/v0.2.0.tar.gz"
+    sha256 "b85e1d87de6caf92d0b4a5965f60e55ff28da5b42cf8288852ff2aece8209756"
+  end
+
+  depends_on "cmake" => :build unless Hardware::CPU.arm?
+  depends_on "pkg-config" => :build unless Hardware::CPU.arm?
+  depends_on "rust" => :build unless Hardware::CPU.arm?
 
   def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+    if Hardware::CPU.arm?
+      bin.install "rsworktree"
+    else
+      system "cargo", "install", *std_cargo_args(path: ".")
+    end
   end
 
   test do
