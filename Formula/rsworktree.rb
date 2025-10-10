075540879c88f68ce68782f970844129afb91faf
Ozan Kaşıkçı <ozan@kasikci.io>
2025-10-10 03:14:27 +0300

    rsworktree 0.6.4

--- Formula/rsworktree.rb
+++ Formula/rsworktree.rb
@@ -1,21 +1,21 @@
 class Rsworktree < Formula
   desc "CLI helper for managing Git worktrees under a dedicated .rsworktree directory"
   homepage "https://github.com/ozankasikci/rust-git-worktree"
-  version "0.6.1"
+  version "0.6.4"
   license "MIT"
   head "https://github.com/ozankasikci/rust-git-worktree.git", branch: "master"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/ozankasikci/rust-git-worktree/releases/download/v0.6.1/rsworktree-v0.6.1-macos-arm64.tar.gz"
-      sha256 "bcba6d7c393c9260e9f053420b5ce4c5f18b629bc7cb3dd1f7f3feccdcd70864"
+      url "https://github.com/ozankasikci/rust-git-worktree/releases/download/v0.6.4/rsworktree-v0.6.4-macos-arm64.tar.gz"
+      sha256 "b1aaaf2dd52dc4882671561b417a5e4bb36b8a0ee99211269418a865170bc4c7"
     else
-      url "https://github.com/ozankasikci/rust-git-worktree/releases/download/v0.6.1/rsworktree-v0.6.1-macos-x86_64.tar.gz"
-      sha256 "a09d65c5f4ecbdfe27ca40357bb7ed412aac327deedf482cbdf5740eb43b7807"
+      url "https://github.com/ozankasikci/rust-git-worktree/archive/refs/tags/v0.6.4.tar.gz"
+      sha256 "6ce3d66cd65202cc75a6e8f4cf06e20f2b9c9b81ac14a9f8506dc96601c6c130"
     end
   else
-    url "https://github.com/ozankasikci/rust-git-worktree/archive/refs/tags/v0.6.1.tar.gz"
-    sha256 "4796db6fe148e4809f3f739d697198af8b573573c91dff2c836f483e3e54cbfb"
+    url "https://github.com/ozankasikci/rust-git-worktree/archive/refs/tags/v0.6.4.tar.gz"
+    sha256 "6ce3d66cd65202cc75a6e8f4cf06e20f2b9c9b81ac14a9f8506dc96601c6c130"
   end
 
   unless OS.mac?
