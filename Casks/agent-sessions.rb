cea85ffe90e27611767f36304e8ada9cf302b3ff
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-03 15:25:51 +0300

    bump agent-sessions to v0.1.6

--- Casks/agent-sessions.rb
+++ Casks/agent-sessions.rb
@@ -1,13 +1,13 @@
 cask "agent-sessions" do
-  version "0.1.5"
+  version "0.1.6"
 
   on_arm do
-    sha256 "777deca60865e778b2a203bea5a2029f56dc5b1192deb327a0198b13b620ffb4"
+    sha256 "8d6162e66a7bd0ab0a3cda4f993490f6e4038599757eb70788f4b6ea530dca91"
     url "https://github.com/ozankasikci/agent-sessions/releases/download/v#{version}/AgentSessions_#{version}_aarch64.dmg"
   end
 
   on_intel do
-    sha256 "98b6fcf2f5bdd42eb21a14889d8544e80dd6d61c912c71179c5ebd04b1fbb9d4"
+    sha256 "f666f6dd70a8d56f246700aeb0f8f8158212f36a2732d11300485a5c54aa0a2f"
     url "https://github.com/ozankasikci/agent-sessions/releases/download/v#{version}/AgentSessions_#{version}_x64.dmg"
   end
 
