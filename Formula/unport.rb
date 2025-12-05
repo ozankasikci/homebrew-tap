74b5e4df4f217830a2bc9371184ddf26b06aedb9
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-05 20:02:52 +0300

    Add unport formula v0.5.0

--- Formula/unport.rb
+++ Formula/unport.rb
@@ -1,11 +1,11 @@
 class Unport < Formula
-  desc "Local development port manager with automatic domain routing"
+  desc "Local development port manager with automatic domain routing and HTTPS support"
   homepage "https://github.com/ozankasikci/unport"
-  version "0.4.1"
+  version "0.5.0"
   license "MIT"
 
-  url "https://github.com/ozankasikci/unport/archive/refs/tags/v0.4.1.tar.gz"
-  sha256 "4b7f708ed27876bea06f1a08ca3dcfe3bc1bddbdbcbafb5c01300054b211f2ad"
+  url "https://github.com/ozankasikci/unport/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "768c61589fb559c03bf3267ae2e60be6ccf3ddae02448509563bd62df5d6fd4a"
 
   depends_on "rust" => :build
 
