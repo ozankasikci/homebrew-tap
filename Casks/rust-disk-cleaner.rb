3260ead98e72b2418634c9355b8c2b40d03d117d
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-06 13:28:49 +0300

    chore: bump rust-disk-cleaner to v0.2.1

--- Casks/rust-disk-cleaner.rb
+++ Casks/rust-disk-cleaner.rb
@@ -1,21 +1,14 @@
 cask "rust-disk-cleaner" do
-  version "0.2.0"
+  version "0.2.1"
+  sha256 "3d6960d2a58e382af93dbeb5942177e466fe4c1094cf6c14b5e4c80d51f93ef7"
 
-  on_arm do
-    sha256 "7467d3c554e803744e9bb41081287203b3d193baf4fe4e355e9e2597d8de9845"
-    url "https://github.com/ozankasikci/rust-disk-cleaner/releases/download/v#{version}/RustDiskCleaner_#{version}_aarch64.dmg"
-  end
+  url "https://github.com/ozankasikci/rust-disk-cleaner/releases/download/v#{version}/RustDiskCleaner_#{version}_aarch64.dmg"
 
-  on_intel do
-    sha256 "863514718f45f9d84eb2840eacdf43a7f70a9a1e6ae77e71bb9f72336dc1c8dd"
-    url "https://github.com/ozankasikci/rust-disk-cleaner/releases/download/v#{version}/RustDiskCleaner_#{version}_x64.dmg"
-  end
+  depends_on arch: :arm64
 
   name "RustDiskCleaner"
   desc "Fast disk space analyzer and cleaner for macOS"
   homepage "https://github.com/ozankasikci/rust-disk-cleaner"
-
-  depends_on macos: ">= :monterey"
 
   app "RustDiskCleaner.app"
 
