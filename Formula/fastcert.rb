ff0392e0aa249aa6025436f0388424893058e87d
Ozan Kaşıkçı <ozan@kasikci.io>
2025-12-10 23:43:46 +0300

    fastcert: update to 0.1.1 with pre-built binaries

--- Formula/fastcert.rb
+++ Formula/fastcert.rb
@@ -1,19 +1,31 @@
 class Fastcert < Formula
   desc "Simple zero-config tool for making locally-trusted development certificates"
   homepage "https://github.com/ozankasikci/fastcert"
-  url "https://github.com/ozankasikci/fastcert/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "2d7563d5757d053d1fec5c24117ef7ffe22d10038a03deaa20bed9128b29bab0"
+  version "0.1.1"
   license "MIT"
-  head "https://github.com/ozankasikci/fastcert.git", branch: "master"
 
-  depends_on "rust" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/ozankasikci/fastcert/releases/download/v0.1.1/fastcert-x86_64-apple-darwin.tar.gz"
+      sha256 "ea64158bcbd3cdf8847e681504b4d4c58e0941983e9380ced6a6994f314e1733"
+    else
+      url "https://github.com/ozankasikci/fastcert/releases/download/v0.1.1/fastcert-aarch64-apple-darwin.tar.gz"
+      sha256 "0f5b7e7c1de027e0b1f2ac79a11b17c33e9937662582d682afa0a18f0e24e974"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/ozankasikci/fastcert/releases/download/v0.1.1/fastcert-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "d27489545509b84d579a481525e533f51e20cd2381579c2873bfc7a4d890d6da"
+    end
+  end
 
   def install
-    system "cargo", "install", *std_cargo_args
+    bin.install "fastcert"
   end
 
   test do
-    # Test that the binary exists and runs
     system "#{bin}/fastcert", "-CAROOT"
   end
 end
